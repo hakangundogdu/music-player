9efa57452b5b5111eecf1e3be1bfa6d7455b6d63
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2022-06-19 23:33:19 +0300

    player buttons

--- src/App.js
+++ src/App.js
@@ -2,35 +2,39 @@ import { useState } from 'react';
 
 import './App.scss';
 
-const PlayButton = () => (
+const PlayButton = (props) => (
   <button
     aria-label="Play button"
     type="button"
     className="player-controls__btn player-controls__play-btn"
+    onClick={props.onClick}
   />
 );
 
-const PauseButton = () => (
+const PauseButton = (props) => (
   <button
     aria-label="Pause button"
     type="button"
     className="player-controls__btn player-controls__pause-btn"
+    onClick={props.onClick}
   />
 );
 
-const NextButton = () => (
+const NextButton = (props) => (
   <button
     aria-label="Next button"
     type="button"
     className="player-controls__btn player-controls__next-btn"
+    onClick={props.onClick}
   />
 );
 
-const PreviousButton = () => (
+const PreviousButton = (props) => (
   <button
     aria-label="Previous button"
     type="button"
     className="player-controls__btn player-controls__prev-btn"
+    onClick={props.onClick}
   />
 );
 
@@ -59,25 +63,24 @@ function App() {
   ];
   const [selectedTrack, setSelectedTrack] = useState({ ...tracks[0] });
 
-  const [isPaused, setPaused] = useState(false);
+  const [isPaused, setPaused] = useState(true);
 
   const pauseHandle = () => {
-    setPaused(true);
-  };
-  const playHandle = () => {
-    setPaused(false);
+    setPaused(() => !isPaused);
   };
 
   const onNextButtonClick = () => {
-    // TODO: Write your code here
-    setSelectedTrack(tracks[selectedTrack.id + 1]);
+    setSelectedTrack(
+      [...tracks].find((track) => track.id === selectedTrack.id + 1) ||
+        tracks[0]
+    );
   };
+
   const onPreviousButtonClick = () => {
-    // TODO: Write your code here
-    if (selectedTrack.id === 0) {
-      setSelectedTrack(tracks[3]);
-    }
-    setSelectedTrack(tracks[selectedTrack.id - 1]);
+    setSelectedTrack(
+      [...tracks].find((track) => track.id === selectedTrack.id - 1) ||
+        tracks[3]
+    );
   };
 
   const renderTracks = () =>
@@ -87,6 +90,10 @@ function App() {
           selectedTrack.id === track.id ? 'tracks-list__item--selected' : ''
         }`}
         key={track.id}
+        onClick={() => {
+          setSelectedTrack(track);
+          setPaused(false);
+        }}
       >
         <span className="tracks-list__name">{track.name}</span>
         <span className="tracks-list__artist">
@@ -99,13 +106,13 @@ function App() {
   return (
     <main>
       <div className="tracks-list">
-        <h2 className="tracks-list__title">Tracks</h2>
+        <h2 className="tracks-list__title">Tracks </h2>
         {renderTracks()}
       </div>
       <div className="player-controls">
-        <PreviousButton />
+        <PreviousButton onClick={onPreviousButtonClick} />
         {isPaused ? (
-          <PlayButton onClick={playHandle} />
+          <PlayButton onClick={pauseHandle} />
         ) : (
           <PauseButton onClick={pauseHandle} />
         )}
